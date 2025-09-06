12866b4dbe44d1b9f88345fca0fbd1f49ac2c086
George Tsouvaltzis <tsouvaltzis.george@gmail.com>
2025-09-06 05:14:13 +0400

    Added streaming support while summarizing

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from application.shared_state import SharedState
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, AIMessageChunk
 from application.workflow.graph import create_workflow_graph
 from langchain_core.runnables import RunnableConfig
 from langgraph.types import Command
@@ -10,21 +10,20 @@ from langgraph.types import Command
 
 def main():
     graph = create_workflow_graph()
-
     config = RunnableConfig(configurable={"thread_id": 1})
-
     human_message = [HumanMessage("I want to purchase a house. I'm making 3k a month and spending 2k a month. I want to have low risk preference and I want to save 50,000$")]
-
     human_message = [HumanMessage("I want to pruchase a house.")]
     state = SharedState(messages=human_message)
-    res = graph.invoke(state, config)
-    print(res)
+
+    graph.invoke(state, config)
 
     while True:
         interrupts = graph.get_state(config).interrupts
         if interrupts:
-            user_response = input(interrupts[0].value)
-            graph.invoke(Command(resume=user_response), config)
+            user_response = input(f"{interrupts[0].value}: ")
+            for token, metadata in graph.stream(Command(resume=user_response), config=config, stream_mode="messages"):
+                if metadata["langgraph_node"] == "summarize" and isinstance(token, AIMessageChunk):
+                    print(token.content, end="")
         else:
             break
 
